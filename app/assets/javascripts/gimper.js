fd314c80b4e0c4b49235141a639beeb66825d148
Lisa Mae <lisamae135b7@me.com>
2017-11-28 22:32:16 -0500

    fixed arrow bugs

--- app/assets/javascripts/gimper.js
+++ app/assets/javascripts/gimper.js
@@ -8,16 +8,37 @@ gimp_sounds = [
 
 gimps_selected = [];
 
-function recreate_arrange_link(gimp_list) {
-  let element_list = [];
-  for (let i=0; i<gimp_list.length; i++) {
-    element_list.push(`gimps[]=${gimp_list[i]}`);
+function link_to_arrange(gimp_list) {
+  if (gimp_list.length > 0) {
+    let element_list = [];
+    for (let i=0; i<gimp_list.length; i++) {
+      element_list.push(`gimps[]=${gimp_list[i]}`);
+    }
+    const element_str = element_list.join('&');
+    window.location.href = `/arrange?${element_str}`;
+  } else {
+    alert('gimpy GIMP GIIIIIIIIMP');
   }
-  const element_str = element_list.join('&');
-  const arrange_link = document.getElementById('arrange_link');
-  arrange_link.href = `/arrange?${element_str}`;
 }
 
+/*
+function recreate_arrange_link(gimp_list) {
+  if (gimp_list) {
+    let element_list = [];
+    for (let i=0; i<gimp_list.length; i++) {
+      element_list.push(`gimps[]=${gimp_list[i]}`);
+    }
+    const element_str = element_list.join('&');
+    const arrange_link = document.getElementById('arrange_link');
+    arrange_link.href = `/arrange?${element_str}`;
+    arrange_link.onclick = null;
+  } else {
+    addEventListener(arrange_link, 'onclick', function() {
+      alert('gimpy GIMP GIIIIIIIIMP');
+    });
+  }
+}
+*/
 function gimp_on_up(gimp_num) {
     const lil_g = document.getElementById('lil_gimper_' + gimp_num);
     const gimp_sound = gimp_sounds[Math.floor(Math.random()*gimp_sounds.length)];
@@ -34,7 +55,7 @@ function gimp_on_up(gimp_num) {
       index = gimps_selected.indexOf(gimp_num);
       gimps_selected.splice(index, 1);
     }
-    recreate_arrange_link(gimps_selected);
+    //recreate_arrange_link(gimps_selected);
 }
 //function dragstart_handler(ev) {
 
